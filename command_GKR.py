cb9def7e35a18ba9a18767a5c072cabc50adb10f
Iverson-cui <yuboiverson@gmail.com>
2025-07-13 16:25:19 +0800

    update multiple copy assignment test finished

--- command_GKR.py
+++ command_GKR.py
@@ -184,6 +184,6 @@ def execute(C):
 
 # C = [circuit.createCircuit("circuitdata-{}.csv".format(i), 10007) for i in range(1, 5)]
 # Deep_C = circuit.createCircuit("deep_circuit-1.csv", 10007)
-test_circuit = circuit.createCircuit(data_dir, [1, 1, 1, 0], 10007)
+test_circuit = circuit.createCircuit(data_dir, [1, 2, 6, 0], 10007)
 execution_time = timeit.timeit(lambda: execute(test_circuit), number=5)
-print("Execution time for test_circuit: ", execution_time / 3, "seconds")
+print("Execution time for test_circuit: ", execution_time / 5, "seconds")
